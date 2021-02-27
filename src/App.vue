bf6d8069cbc5dbc5052a47e3db097b5771259b9d
fujimura-piano <fujimura.piano.lesson@gmail.com>
2021-02-27 17:57:32 +0900

    Webpackつかってみた

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+
+    <nav class="navbar navbar-expand-lg navbar-light" id="main-nav">
+      <a class="navbar-brand" href="/">ふじむらピアノ教室</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link">ホーム</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" class="nav-link">講師紹介</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <router-view></router-view>
+    
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <main role="main" class="container" id="main">
+    
+    <header class="jumbotron" id="header">
+      <h2>岩国市岩国・岩国小学校近く</h2>
+      <h1 class="display-4">ふじむらピアノ教室</h1>
+    </header>
+
+    <section class="row">
+      <p>ピアノレッスン</p>
+    </section>
+
+
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
